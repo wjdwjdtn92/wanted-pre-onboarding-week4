e548e12e8d892afb9fbb632e936ca0505f17e154
Jung Su <wjdwjdtn92@naver.com>
2023-05-15 17:26:23 +0900

    [Feat] ALERT_MESSAGE 추가

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -3,3 +3,8 @@ export const API_ERROR_MESSAGE = {
   CREATE_TODO: 'API createTodo error',
   DELETE_TODO: 'API deleteTodo error',
 } as const;
+
+export const ALERT_MESSAGE = {
+  EMPTY: 'Please write something',
+  ERROR: 'Something went wrong.',
+};
