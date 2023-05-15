5d09ccf6ec9c7fcac4eda6893ee485e0d0cb35d4
Jung Su <wjdwjdtn92@naver.com>
2023-05-15 16:37:17 +0900

    [Refacotr] TodoListProps 추가

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,13 @@
+import React from 'react';
+import { TodoType } from '../types';
 import TodoItem from './TodoItem';
 
-function TodoList({ todos, setTodos }) {
+type TodoListProps = {
+  todos: TodoType[];
+  setTodos: React.Dispatch<React.SetStateAction<TodoType[]>>;
+};
+
+function TodoList({ todos, setTodos }: TodoListProps) {
   return todos.length ? (
     <ul>
       {todos.map(({ id, title }) => (
