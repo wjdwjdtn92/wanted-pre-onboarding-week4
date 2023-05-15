7cff65ac023f09f65ddbb44a1258b8b212b77d1d
Jung Su <wjdwjdtn92@naver.com>
2023-05-15 16:40:56 +0900

    [Refactor] api request type 적용

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,33 +1,38 @@
-import apiRequest from "./index";
+import apiRequest from './index';
 
-const RESOURCE = "/todos";
+const RESOURCE = '/todos';
 
 export const getTodoList = async () => {
   try {
-    const response = await apiRequest.get(`${RESOURCE}`);
+    const response = await apiRequest.get({ url: `${RESOURCE}` });
 
     return response;
   } catch (error) {
-    throw new Error("API getTodoList error");
+    throw new Error('API getTodoList error');
   }
 };
 
-export const createTodo = async (data) => {
+export const createTodo = async (title: string) => {
   try {
-    const response = await apiRequest.post(`${RESOURCE}`, data);
+    const response = await apiRequest.post({
+      url: `${RESOURCE}`,
+      data: {
+        title,
+      },
+    });
 
     return response;
   } catch (error) {
-    throw new Error("API createTodo error");
+    throw new Error('API createTodo error');
   }
 };
 
-export const deleteTodo = async (id) => {
+export const deleteTodo = async (id: string) => {
   try {
-    const response = await apiRequest.delete(`${RESOURCE}/${id}`);
+    const response = await apiRequest.delete({ url: `${RESOURCE}/${id}` });
 
     return response;
   } catch (error) {
-    throw new Error("API deleteTodo error");
+    throw new Error('API deleteTodo error');
   }
 };
