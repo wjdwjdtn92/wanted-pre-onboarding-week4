ddb7c6103575f8e0cca7b828e36dd7e300754abd
Jung Su <wjdwjdtn92@naver.com>
2023-05-15 16:38:07 +0900

    [Refactor] useState에 TodoType[] 타입 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,9 +4,10 @@ import Header from '../components/Header';
 import InputTodo from '../components/InputTodo';
 import TodoList from '../components/TodoList';
 import { getTodoList } from '../api/todo';
+import { TodoType } from '../types';
 
 function Main() {
-  const [todoListData, setTodoListData] = useState([]);
+  const [todoListData, setTodoListData] = useState<TodoType[]>([]);
 
   useEffect(() => {
     (async () => {
